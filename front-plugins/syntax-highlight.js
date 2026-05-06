bbbab60ac8dbb02ed0350e640c9ffacaf3d69482
Rafael Aznar <rafaaznar@gmail.com>
2026-05-06 11:14:01 +0200

    Refactor code structure for improved readability and maintainability

--- front-plugins/syntax-highlight.js
+++
@@ -1,3 +0,0 @@
-// Syntax highlighting has been integrated into the wiki core (wiki.js + index.php).
-// The admin can select the highlight.js theme in the Settings panel.
-// This file is intentionally empty and can be safely deleted.
